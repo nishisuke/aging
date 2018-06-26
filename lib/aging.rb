de571dcd81ccb007f6f356b80d8ae6d5601705c1
nishisuke <nishisuke13+git@gmail.com>
2018-06-26 20:52:10 +0900

    Release v0.1.0

--- lib/aging.rb
+++ lib/aging.rb
@@ -1,5 +1,30 @@
+require 'date'
+
 require "aging/version"
 
 module Aging
-  # Your code goes here...
+  refine Date do
+    def count_year
+      # alias self
+      from_date = self
+      klass = self.class
+      to_date = klass.today
+      # TODO: error
+      raise if to_date < from_date
+
+      birthday_in_this_year = klass.new(
+        to_date.year,
+        from_date.month,
+        from_date.day
+      )
+
+      if to_date < birthday_in_this_year
+        # before
+        to_date.year - from_date.year - 1
+      else
+        # after
+        to_date.year - from_date.year
+      end
+    end
+  end
 end
